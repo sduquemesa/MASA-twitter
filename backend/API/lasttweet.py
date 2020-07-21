62a101ecadc14e6e54fbd97c6d23b8ef53d112a3
sduquemesa <675763+sduquemesa@users.noreply.github.com>
2020-07-20 22:29:17 -0500

    API endpoint

--- backend/API/lasttweet.py
+++ backend/API/lasttweet.py
@@ -8,13 +8,13 @@ DB_FILE = Path("../twitter_streaming/") / 'tweets.sqlite'
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
 
-def get_db_data() -> dict:
+def get_db_data() -> pd.DataFrame:
 
     con = sqlite3.connect(DB_FILE)
     statement = 'SELECT * FROM tweets'
     df = pd.read_sql_query(statement, con)
 
-    return df.to_dict()
+    return df
 
 # Create a handler for our read (GET) lasttweet
 def read():
@@ -26,5 +26,11 @@ def read():
     """
 
     tweet_data = get_db_data()
+    api_data_dict = {
+        'created_at': tweet_data['created_at'][0],
+        'fetched_at': get_timestamp(),
+        'coords': tweet_data['coords'][0],
+        'html_data': tweet_data['html'][0]
+    }
     
-    return dict([['{}'.format(key),tweet_data[key][0]] for key in tweet_data.keys()])
+    return api_data_dict
